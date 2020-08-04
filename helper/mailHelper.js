9b2908e7b0d1fca5416d591a785fd1e228fd878f
bobkombat <rdhwnrmdhny@gmail.com>
2020-08-04 22:15:36 +0700

    fix bug

--- helper/mailHelper.js
+++ helper/mailHelper.js
@@ -15,12 +15,12 @@ module.exports = async (email, data, options) => {
     subject: `${options} a todo list with id of ${data.id}`,
     text: "Hello world!",
     html: `<h5>${options} a todo list</h5>
-    <ul>
-      <li>${data.title}</li>
-      <li>${data.description}</li>
-      <li>${data.status}</li>
-      <li>${data.due_date}</li>
-    </ul>`
+<ul>
+  <li>${data.title}</li>
+  <li>${data.description}</li>
+  <li>${data.status}</li>
+  <li>${data.due_date}</li>
+</ul>`
   }
 
   return await transporter.sendMail(mailOption)
