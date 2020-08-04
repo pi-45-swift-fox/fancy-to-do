fe7cf00e0a34f3b8ace7131edcf662996eb70cea
Klaustin Wijaya <klaustin.wijaya@gmail.com>
2020-08-04 12:06:43 +0700

    Make some changes to middlewares/index

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,38 +1,41 @@
-const {User} = require('../models');
+const {User, Todo} = require('../models');
 const jwt = require('jsonwebtoken');
 
 class Auth {
-    static check(req, res, next) {
+    static async check(req, res, next) {
         try {
             const userToken = jwt.verify(
                 req.headers.accesstoken,
                 process.env.JWT_SECRET
             );
 
-            User.findOne({
+            let data = await User.findOne({
                 where: {
                     username: userToken.username
                 }
             })
-            .then(data => {
-                req.userLogin = data;
-                next();
-            })
+
+            req.userLogin = data;
+            next();
 
         } catch (error) {
             console.log(error);
-            res.status(401).json({msg: 'Unable to authenticate'});
+            res.status(500).json({msg: 'Unable to authenticate'});
         }
     }
 
-    static checkUser(req, res, next) {
+    static async checkUser(req, res, next) {
         try {
-            const data = await User.findByPk(+req.params.id);
+            const data = await Todo.findByPk(+req.params.id);
 
-            if(data.id === req.userLogin.id) {
-                next();
+            if(data) {
+                if(data.UserId === req.userLogin.id) {
+                    next();
+                } else {
+                    res.status(403).json('Access denied');
+                }
             } else {
-                res.status(403).json('Access denied');
+                res.status(404).json(`Unable to find the todo with this id:${+req.params.id}`)
             }
             
         } catch (error) {
