605f8d2e9d50fb26ba51d5642734020cfa0e9c63
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2020-08-05 04:16:23 +0700

    tugas2 & 3

--- routes/todos-route.js
+++ routes/todos-route.js
@@ -7,8 +7,8 @@ const authorization=require('../middleware/authorization')
 route.post('/',authentication,Constroller.postData)
 route.get('/',authentication,Constroller.readData)
 route.get('/:id',authentication,authorization,Constroller.readById)
-route.put('/:id',Constroller.update)
-route.delete('/:id',Constroller.del)
+route.put('/:id',authentication,authorization,Constroller.update)
+route.delete('/:id',authentication,authorization,Constroller.del)
 
 
 
